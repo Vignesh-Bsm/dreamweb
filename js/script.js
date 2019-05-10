244d00028ef56688a0934d805f10484a0dbb3c26
Vignesh Bsm <bsmvignesh95@gmail.com>
2019-05-10 11:58:15 +0530

    initial-commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$(window).on('load', function () {
+    $('#status').fadeOut();
+    $('#preloader').delay(350).fadeOut();
+});
+
+
+// ============== Team ===================
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
